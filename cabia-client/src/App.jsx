4f1aa00e48410a17e13eac4bbbb92272b63e0fb5
Nash Cabia <cabians@student.national-u.edu.ph>
2026-03-25 11:48:31 +0800

    Initial commit

---
+++ cabia-client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>
+          Name: Nash Daniel Cabia <br />
+          Email: cabianash@gmail.com <br />
+          Course: BSIT <br />
+          Section: INF231 <br />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
