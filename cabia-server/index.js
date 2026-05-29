7c27377d7b92ffd8951588bbb9719d7038cecfe5
Nash Cabia <cabians@student.national-u.edu.ph>
2026-05-29 19:51:38 +0800

    Add health check routes

--- cabia-server/index.js
+++ cabia-server/index.js
@@ -33,3 +33,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("CABIA Backend is running");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "CABIA API is running" });
+});
